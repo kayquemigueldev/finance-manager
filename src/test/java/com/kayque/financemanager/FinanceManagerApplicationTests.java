abf8af5a0e22d11b1143b58e70c4d6ee09321e52
kayquemigueldev <kayquemiguel.dev@gmail.com>
2026-06-29 20:53:11 -0300

    chore: initialize finance manager project

---
+++ src/test/java/com/kayque/financemanager/FinanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kayque.financemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
