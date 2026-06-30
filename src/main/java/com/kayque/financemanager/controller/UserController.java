642ffea90d531c4ed13ba6345eed103cb23cc444
kayquemigueldev <kayquemiguel.dev@gmail.com>
2026-06-29 21:26:15 -0300

    feat: create user controller

---
+++ src/main/java/com/kayque/financemanager/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.kayque.financemanager.controller;
+
+import com.kayque.financemanager.dto.CreateUserRequest;
+import com.kayque.financemanager.dto.UserResponse;
+import com.kayque.financemanager.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponse create(@RequestBody @Valid CreateUserRequest request) {
+        return userService.create(request);
+    }
+}
