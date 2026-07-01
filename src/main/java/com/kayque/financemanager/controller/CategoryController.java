451e7d421532c40fb21d1b8581b52f9e84e95ffc
kayquemigueldev <kayquemiguel.dev@gmail.com>
2026-06-30 21:39:02 -0300

    feat: expose category REST endpoints

--- src/main/java/com/kayque/financemanager/controller/CategoryController.java
+++ src/main/java/com/kayque/financemanager/controller/CategoryController.java
@@ -1,4 +1,41 @@
 package com.kayque.financemanager.controller;
 
+import com.kayque.financemanager.dto.CategoryRequest;
+import com.kayque.financemanager.dto.CategoryResponse;
+import com.kayque.financemanager.service.CategoryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+@RequiredArgsConstructor
 public class CategoryController {
+
+    private final CategoryService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryResponse create(@RequestBody @Valid CategoryRequest request) {
+        return service.create(request);
+    }
+
+    @GetMapping
+    public List<CategoryResponse> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public CategoryResponse findById(@PathVariable Long id) {
+        return service.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
 }
