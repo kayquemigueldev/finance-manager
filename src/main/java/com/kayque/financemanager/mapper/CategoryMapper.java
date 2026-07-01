97c6955449f1cae016a925ed3be919847d17ffe4
kayquemigueldev <kayquemiguel.dev@gmail.com>
2026-06-30 21:32:48 -0300

    feat: implement category service layer

--- src/main/java/com/kayque/financemanager/mapper/CategoryMapper.java
+++ src/main/java/com/kayque/financemanager/mapper/CategoryMapper.java
@@ -1,4 +1,29 @@
 package com.kayque.financemanager.mapper;
 
+import com.kayque.financemanager.dto.CategoryRequest;
+import com.kayque.financemanager.dto.CategoryResponse;
+import com.kayque.financemanager.entity.Category;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CategoryMapper {
+
+    public Category toEntity(CategoryRequest request) {
+
+        return Category.builder()
+                .name(request.name())
+                .type(request.type())
+                .build();
+    }
+
+    public CategoryResponse toResponse(Category category) {
+
+        return new CategoryResponse(
+                category.getId(),
+                category.getName(),
+                category.getType(),
+                category.getCreatedAt(),
+                category.getUpdatedAt()
+        );
+    }
 }
--- src/main/java/com/kayque/financemanager/service/CategoryService.java
+++ src/main/java/com/kayque/financemanager/service/CategoryService.java
@@ -1,4 +1,49 @@
 package com.kayque.financemanager.service;
 
+import com.kayque.financemanager.dto.CategoryRequest;
+import com.kayque.financemanager.dto.CategoryResponse;
+import com.kayque.financemanager.entity.Category;
+import com.kayque.financemanager.mapper.CategoryMapper;
+import com.kayque.financemanager.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    private final CategoryRepository repository;
+    private final CategoryMapper mapper;
+
+    public CategoryResponse create(CategoryRequest request) {
+
+        Category category = mapper.toEntity(request);
+
+        category = repository.save(category);
+
+        return mapper.toResponse(category);
+    }
+
+    public List<CategoryResponse> findAll() {
+
+        return repository.findAll()
+                .stream()
+                .map(mapper::toResponse)
+                .toList();
+    }
+
+    public CategoryResponse findById(Long id) {
+
+        Category category = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+
+        return mapper.toResponse(category);
+    }
+
+    public void delete(Long id) {
+
+        repository.deleteById(id);
+    }
 }
